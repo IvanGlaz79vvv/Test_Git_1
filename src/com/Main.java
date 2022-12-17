5e3e80533074175aa273ecaf921e9dd1531bc986
IvanGlaz79vvv <ivangl79@gmail.com>
2022-12-17 16:47:49 +0300

    Initial commit

---
+++ src/com/Main.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("\nHello world!");
+
+        BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
+        String input = reader.readLine();
+        System.out.println(input);
+
+        input = reader.readLine();
+        System.out.println(input);
+
+        input = reader.;
+        System.out.println(input);
+    }
+}
